6b55a2a48ad8aa4072d23d8dd0824a01192dc624
Sara Oliver <s.oliver1@icloud.com>
2019-11-11 00:31:34 +0000

    changed colours

--- js/script.js
+++ js/script.js
@@ -1,26 +1,32 @@
-$(document).ready(function() {
-    -$("#stream1_btn").on("click", function() {
-        $(".graph1").show();
-        $(".graph1").show(1000);
+    $(document).ready(function() {
+      -$("#barchart_1").show() 
+        $("#barchart_1").show(1000);
         $("#button_effects1").click(function(){
         $('#button_effects1').hide('slow');
+        });
     });
- });
- 
-   $("#stream2_btn").on("click", function() {
+
+   $("#barchart2_html").on("click", function() {
         $(".graph1").removeClass('highlight_graph');
         $(".graph2").removeClass('highlight_graph');
         $(".graph3").removeClass('highlight_graph');
         $(".graph2").addClass('highlight_graph');
    });
 
-    $("#stream3_btn").on("click", function() {
+    $("#barchart3_html").on("click", function() {
         $(".graph1").removeClass('highlight_graph');
         $(".graph2").removeClass('highlight_graph');
         $(".graph3").removeClass('highlight_graph');
         $(".graph3").addClass('highlight_graph');
-    }); 
-});     
+    });
+
+    $("#barchart4_html").on("click", function() {
+        $(".graph2").removeClass('highlight_graph');
+        $(".graph3").removeClass('highlight_graph');
+        $(".graph4").removeClass('highlight_graph');
+        $(".graph4").addClass('highlight_graph');
+    });
+
 
     $(document).ready(function() {
     $("#button_effects1").click(function(){
@@ -35,24 +41,18 @@ $(document).ready(function() {
     $("#button_effects4").click(function(){
         $('#par4').toggle('1000');
     });
-}); 
+});$(document).ready(function() {
+    -$("#barchart_html").on("click", function() {
+  
 
-    $(document).ready(function() {
+$(document).ready(function() {
     // Create the slideToggle effects each of the paragraphs and
     // buttons
-    $("#button_effects1").click(function(){
-        $('#par1').slideToggle('1000');
-    });
-    $("#button_effects2").click(function(){
-        $('#par2').slideToggle('1000');
-    });
-    $("#button_effects3").click(function(){
-        $('#par3').slideToggle('1000');
-    });
+
     $("#button_effects4").click(function(){
         $('#par4').slideToggle('1000');
     });
-});  
+});
 
 // Use the fadeTo effect when the mouse hovers over a specific button
     // and fadeTo again when the mouse is no longer hovering over the button
@@ -81,68 +81,39 @@ $(document).ready(function() {
     $("#button_effects4").mouseleave(function(){
         $('#button_effects4').fadeTo(1000, 1);
     });
-    $("#button_effects5").mouseenter(function(){
-        $('#button_effects5').fadeTo(1000, 0.5);
-    });
-    $("#button_effects5").mouseleave(function(){
-        $('#button_effects5').fadeTo(1000, 1);
-    });
-    $("#button_effects6").mouseenter(function(){
-        $('#button_effects6').fadeTo(1000, 0.5);
-    });
-    $("#button_effects6").mouseleave(function(){
-        $('#button_effects6').fadeTo(1000, 1);
-    });
+
 //waits until page is loaded first
 
     $(document).ready(function(){
-    //applies colour red to paragraphs when clicked on 
+    //applies colour red to paragraphs when clicked on
     $("p").click(function(){
         $("p").addClass("highlight_text");
     });
-});    
+});
 
 //will add lightblue to h2 elements
 
     $("h2").hover(function(){
-        $("h2").addClass( "h2_color");   
+        $("h2").addClass( "h2_color");
     });
-    
+
     /*
-    this will apply larger font size to the active h2 element 
-    by adding the h2_font_size class but 
+    this will apply larger font size to the active h2 element
+    by adding the h2_font_size class but
     not the other h2 elements by removing class h2_font_size from them
     */
 
     $("#hr_html").hover(function(){
-        $("#hr_graph").removeClass("h2_font_size");
-        $("#hr_dashboard").removeClass("h2_font_size");
+        $("#hr_barchart").removeClass("h2_font_size");
+        $("#hr_barchart2").removeClass("h2_font_size");
         $("#hr_scatterplot").removeClass("h2_font_size");
         $("#hr_piechart").removeClass("h2_font_size");
-        $("#hr_css").removeClass("h2_font_size");
-        $("#hr_mysql").removeClass("h2_font_size");
-        $("#hr_html").addClass("h2_font_size");
-    }); 
-    
-    $("#hr_css").hover(function(){
-        $("#hr_mysql").removeClass("h2_font_size");
-        $("#hr_python").removeClass("h2_font_size");
-        $("#hr_jquery").removeClass("h2_font_size");
-        $("#hr_django").removeClass("h2_font_size");
-        $("#hr_html").removeClass("h2_font_size");
-        $("#hr_css").addClass("h2_font_size");
     });
-    
+
+
     //applies colour black to body background when mouse enters over buttons
 
-
     $(".bottom_button").mouseenter(function(){
-        $("body").css( "background-color", "black"); 
+        $("body").css( "background-color", "black");
     });
 
-    //applies colour grey to body background when mouse leaves buttons
-
-    $(".bottom_button").mouseleave(function(){
-        $("body").css( "background-color", "#eee"); 
-    });
- 
